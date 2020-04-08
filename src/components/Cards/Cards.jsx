23fcad1ed325417ffa5e40bd54d96bed922b9c31
roy1210 <rswkure@gmail.com>
2020-04-08 20:34:32 +0800

    test: cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -5,70 +5,11 @@ import styles from './Cards.module.css'
 import cx from 'classnames'
 
 const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate, stillInfected } }) => {
-
-  const cards = confirmed && [
-    {
-      title: 'Total Infected',
-      class: styles.infected,
-      end: confirmed.value,
-      text: 'Number of patient still infected of COVID-19'
-    },
-    {
-      title: 'Still Infected',
-      class: styles.infecting,
-      end: stillInfected,
-      text: 'Number of patient still infected COVID-19'
-    },
-    {
-      title: 'Recovered',
-      class: styles.recovered,
-      end: recovered.value,
-      text: 'Number of patient recovered from COVID-19'
-    },
-    {
-      title: 'Deaths',
-      class: styles.deaths,
-      end: deaths.value,
-      text: 'Number of patient killed by COVID-19'
-    },
-  ]
-
-  // if (!confirmed) {
-  //   return 'Loading...'
-  // } else {
   return (
-    // Memo: cx: apply multiple classes
-
-    <div className={styles.container}>
-      <Grid container spacing={3} justify="center">
-        <h1>test</h1>
-        {/* {confirmed && cards.map(card => (
-            <Grid item xs={12} md={2} component={Card} className={cx(styles.card, card.class)} key={card.title} >
-              <CardContent>
-                <Typography color="textSecondary" gutterBottom>
-                  {card.title}
-                </Typography>
-                <Typography variant="h5">
-                  <CountUp
-                    start={0}
-                    end={card.end}
-                    duration={2.5}
-                    separator=","
-                  />
-                </Typography>
-                <Typography color="textSecondary">
-                  {new Date(lastUpdate).toDateString()}
-                </Typography>
-                <Typography variant="body2">
-                  {card.text}
-                </Typography>
-              </CardContent>
-            </Grid>
-          ))} */}
-      </Grid>
-    </div>
+    <>
+      <h1>Test</h1>
+    </>
   )
 }
-// }
 
 export default Cards
